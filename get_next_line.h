394c7fd1e112431e575d50e10abc709393165584
Patumwanpang <pun_yavee@live.com>
2022-11-26 14:23:57 +0700

    edit

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 14:21:38 by puttasa           #+#    #+#             */
+/*   Updated: 2022/11/26 14:23:39 by puttasa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
